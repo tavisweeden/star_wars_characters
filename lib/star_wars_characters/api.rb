816a51c70e2166e8810b1bcd5dd9222c6ff43b43
github username <github email address>
2019-12-14 18:34:01 -0500

    Refined API scraper method

--- lib/star_wars_characters/api.rb
+++ lib/star_wars_characters/api.rb
@@ -1,8 +1,14 @@
 class StarWarsCharacters::API
-    def self.fetch(query)
-        url = "https://swapi.co/api"
-        results = HTTparty.get("#{url}/people/query=#{query}")
+#     def self.fetch(query)
+#         url = "https://swapi.co/api/people/"
+#         results = HTTparty.get("https://swapi.co/api/people/#{url}/?search=/query=#{query}")
+#     end
+# end
+
+    def self.fetch
+        url = "https://swapi.co/api/people/"
+        results = HTTParty.get(url)
+binding.pry 
     end
+
 end
-
-
